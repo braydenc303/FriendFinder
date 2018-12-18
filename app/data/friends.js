14d28ada52f08b48adb55d7412392ea346fc568d
Brayden Copley <braydenc303@gmail.com>
2018-12-17 22:34:14 -0700

    Home and survey pages built, initial data array created.

---
+++ app/data/friends.js
@@ -0,0 +1,263 @@
+var sadMen = [
+    {
+        name: "Keanu",
+        photo: "https://i.kym-cdn.com/entries/icons/original/000/002/862/SadKeanu.jpg",
+        scores: [
+            // I wish people would be more excellent to each other.
+            5,
+            // I do not find it easy being green.
+            5,
+            // The Dolphins make me cry.
+            3,
+            //I would buy myself a grey guitar.
+            4,
+            //I just can't make up my mind who I want to be.
+            2,
+            //No one knows what it's like to be the sad man.
+            4,
+            //My old friend Matt does a much better job at this.
+            1,
+            //No, but I have a very particular set of skills.
+            1,
+            //Sometimes I say more than I should.
+            2,
+            //Maybe I should pay my taxes.
+            5
+        ]
+    },
+    {
+        name: "Kermit",
+        photo: "https://i.imgflip.com/22zzru.jpg",
+        scores: [
+            // I wish people would be more excellent to each other.
+            5,
+            // I do not find it easy being green.
+            5,
+            // The Dolphins make me cry.
+            2,
+            //I would buy myself a grey guitar.
+            1,
+            //I just can't make up my mind who I want to be.
+            1,
+            //No one knows what it's like to be the sad man.
+            4,
+            //My old friend Matt does a much better job at this.
+            1,
+            //No, but I have a very particular set of skills.
+            1,
+            //Sometimes I say more than I should.
+            3,
+            //Maybe I should pay my taxes.
+            5
+        ]
+    },
+    {
+        name: "Darius",
+        photo: "https://www.billboard.com/files/media/Darius-Rucker-press-photo-2017-bb24-billboard-1548.jpg",
+        scores: [
+            // I wish people would be more excellent to each other.
+            5,
+            // I do not find it easy being green.
+            5,
+            // The Dolphins make me cry.
+            5,
+            //I would buy myself a grey guitar.
+            1,
+            //I just can't make up my mind who I want to be.
+            3,
+            //No one knows what it's like to be the sad man.
+            4,
+            //My old friend Matt does a much better job at this.
+            1,
+            //No, but I have a very particular set of skills.
+            1,
+            //Sometimes I say more than I should.
+            5,
+            //Maybe I should pay my taxes.
+            5
+        ]
+    },
+    {
+        name: "Adam",
+        photo: "http://theindiespiritualist.com/wp-content/uploads/2012/06/Adam-Duritz-4.jpg",
+        scores: [
+            // I wish people would be more excellent to each other.
+            5,
+            // I do not find it easy being green.
+            5,
+            // The Dolphins make me cry.
+            3,
+            //I would buy myself a grey guitar.
+            5,
+            //I just can't make up my mind who I want to be.
+            2,
+            //No one knows what it's like to be the sad man.
+            5,
+            //My old friend Matt does a much better job at this.
+            3,
+            //No, but I have a very particular set of skills.
+            1,
+            //Sometimes I say more than I should.
+            2,
+            //Maybe I should pay my taxes.
+            5
+        ]
+    },
+    {
+        name: "Brad",
+        photo: "http://wallstreetnation.com/wp-content/uploads/2017/01/wsnpic9-1.jpg",
+        scores: [
+            // I wish people would be more excellent to each other.
+            3,
+            // I do not find it easy being green.
+            3,
+            // The Dolphins make me cry.
+            3,
+            //I would buy myself a grey guitar.
+            3,
+            //I just can't make up my mind who I want to be.
+            3,
+            //No one knows what it's like to be the sad man.
+            3,
+            //My old friend Matt does a much better job at this.
+            3,
+            //No, but I have a very particular set of skills.
+            3,
+            //Sometimes I say more than I should.
+            3,
+            //Maybe I should pay my taxes.
+            3
+        ]
+    },
+    {
+        name: "Roger",
+        photo: "https://riotfest.org/wp-content/uploads/2016/10/roger-daltrey.jpg",
+        scores: [
+            // I wish people would be more excellent to each other.
+            1,
+            // I do not find it easy being green.
+            5,
+            // The Dolphins make me cry.
+            3,
+            //I would buy myself a grey guitar.
+            5,
+            //I just can't make up my mind who I want to be.
+            1,
+            //No one knows what it's like to be the sad man.
+            5,
+            //My old friend Matt does a much better job at this.
+            1,
+            //No, but I have a very particular set of skills.
+            1,
+            //Sometimes I say more than I should.
+            5,
+            //Maybe I should pay my taxes.
+            3
+        ]
+    },
+    {
+        name: "Ben",
+        photo: "http://i.imgur.com/XqJjGjx.jpg",
+        scores: [
+            // I wish people would be more excellent to each other.
+            3,
+            // I do not find it easy being green.
+            5,
+            // The Dolphins make me cry.
+            3,
+            //I would buy myself a grey guitar.
+            1,
+            //I just can't make up my mind who I want to be.
+            2,
+            //No one knows what it's like to be the sad man.
+            3,
+            //My old friend Matt does a much better job at this.
+            5,
+            //No, but I have a very particular set of skills.
+            1,
+            //Sometimes I say more than I should.
+            2,
+            //Maybe I should pay my taxes.
+            5
+        ]
+    },
+    {
+        name: "Liam",
+        photo: "https://hips.hearstapps.com/esq.h-cdn.co/assets/15/08/1424658502-liam.jpg?resize=480:*",
+        scores: [
+            // I wish people would be more excellent to each other.
+            1,
+            // I do not find it easy being green.
+            5,
+            // The Dolphins make me cry.
+            1,
+            //I would buy myself a grey guitar.
+            1,
+            //I just can't make up my mind who I want to be.
+            1,
+            //No one knows what it's like to be the sad man.
+            4,
+            //My old friend Matt does a much better job at this.
+            1,
+            //No, but I have a very particular set of skills.
+            5,
+            //Sometimes I say more than I should.
+            3,
+            //Maybe I should pay my taxes.
+            5
+        ]
+    },
+    {
+        name: "Elon",
+        photo: "http://cdn01.dailycaller.com/wp-content/uploads/2018/02/Frowning_Elon_Musk-e1518814655860.jpg",
+        scores: [
+            // I wish people would be more excellent to each other.
+            1,
+            // I do not find it easy being green.
+            5,
+            // The Dolphins make me cry.
+            1,
+            //I would buy myself a grey guitar.
+            2,
+            //I just can't make up my mind who I want to be.
+            1,
+            //No one knows what it's like to be the sad man.
+            2,
+            //My old friend Matt does a much better job at this.
+            1,
+            //No, but I have a very particular set of skills.
+            1,
+            //Sometimes I say more than I should.
+            5,
+            //Maybe I should pay my taxes.
+            1
+        ]
+    },
+    {
+        name: "Willie",
+        photo: "http://somethingelsereviews.com/wp-content/uploads/2013/09/willie_nelson.jpg",
+        scores: [
+            // I wish people would be more excellent to each other.
+            5,
+            // I do not find it easy being green.
+            5,
+            // The Dolphins make me cry.
+            3,
+            //I would buy myself a grey guitar.
+            1,
+            //I just can't make up my mind who I want to be.
+            1,
+            //No one knows what it's like to be the sad man.
+            1,
+            //My old friend Matt does a much better job at this.
+            5,
+            //No, but I have a very particular set of skills.
+            3,
+            //Sometimes I say more than I should.
+            2,
+            //Maybe I should pay my taxes.
+            1
+        ]
+    },
+
+]
